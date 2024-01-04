00a86f0f14cfd7b31016002d2b47823ddd8e0a4e
luisFebro <mr.febro@gmail.com>
2024-01-04 02:50:49 -0300

    updating project

--- app/src/main/java/com/febro/kotlinflowsplayground/MainActivity.kt
+++ app/src/main/java/com/febro/kotlinflowsplayground/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -17,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.febro.kotlinflowsplayground.ui.theme.KotlinFlowsPlaygroundTheme
+import kotlinx.coroutines.flow.collect
 
 // ref: https://www.youtube.com/playlist?list=PLQkwcJG4YTCQHCppNAQmLsj_jW38rU9sC
 class MainActivity : ComponentActivity() {
@@ -25,16 +28,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             KotlinFlowsPlaygroundTheme {
                 val viewModel by viewModels<MainViewModel>()
-                
-                val time by viewModel.countDownFlow.collectAsState(initial = 10)
 
+                val count by viewModel.stateFlow.collectAsState()
+                // val time by viewModel.countDownFlow.collectAsState(initial = 10)
+
+                // exp: https://youtu.be/za-EEkqJLCQ?t=1326
+                LaunchedEffect(key1 = true) {
+                    viewModel.sharedFlow.collect { number ->
+
+                    }
+                }
 
                 Box(modifier = Modifier.fillMaxSize()) {
-                    Text(
-                        text = time.toString(),
-                        fontSize =  30.sp,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
+                    Button(onClick = { viewModel.incrementCounter() }) {
+                        Text(text = "Counter: $count")
+                    }
+//                    Text(
+//                        text = time.toString(),
+//                        fontSize =  30.sp,
+//                        modifier = Modifier.align(Alignment.Center)
+//                    )
                 }
             }
         }
--- app/src/main/java/com/febro/kotlinflowsplayground/MainViewModel.kt
+++ app/src/main/java/com/febro/kotlinflowsplayground/MainViewModel.kt
@@ -1,14 +1,16 @@
 package com.febro.kotlinflowsplayground
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.input.key.Key.Companion.I
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     val countDownFlow = flow<Int> {
         val startingValue = 10
@@ -16,7 +18,7 @@ class MainViewModel: ViewModel() {
 
         emit(startingValue)
 
-        while(currentValue > 0) {
+        while (currentValue > 0) {
             delay(1000L)
             currentValue--
 
@@ -24,18 +26,115 @@ class MainViewModel: ViewModel() {
         }
     }
 
+    private val _stateFlow = MutableStateFlow(0)
+    val stateFlow = _stateFlow.asStateFlow()
+
+    private val _sharedFlow = MutableSharedFlow<Int>(5)
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
     init {
-        collectFlow()
+        // collectFlow()
+
+        viewModelScope.launch {
+            sharedFlow.collect {
+                delay(2000L)
+                println("@@ First SharedFlow: The received number is $it")
+            }
+        }
+
+        viewModelScope.launch {
+            sharedFlow.collect {
+                delay(3000L)
+                println("@@ Second SharedFlow: The received number is $it")
+            }
+        }
+
+        squareNumber(2)
+    }
+
+    fun squareNumber(number: Int) {
+        viewModelScope.launch {
+            _sharedFlow.emit(number * number)
+        }
+    }
+
+    fun incrementCounter() {
+        _stateFlow.value += 1
     }
 
     private fun collectFlow() {
+//        countDownFlow.onEach {
+//            println("The current time is $it (ONEACH)")
+//        }.launchIn(viewModelScope)
+//        val flow1 = flow<Int> {
+//            emit(1)
+//            delay(500L)
+//            emit(2)
+//        }
+
+        // val flow1 = (1..5).asFlow()
+
+        val flowRestaurant = flow {
+            delay(50L)
+            emit("Appetizer")
+
+            delay(1000L)
+            emit("Main Dish")
+
+            delay(100L)
+            emit("Dessert")
+
+        }
+
         viewModelScope.launch {
-            // latsetLatest
-            countDownFlow.collectLatest { time ->
-                // prints only the last value and others are cancelled
-                delay(1500L)
-                println("The current time is $time")
+            flowRestaurant.onEach {
+                println("@@ $it is delivered")
             }
+                .buffer()
+                .collect {
+                println("@@ Now eating $it")
+                delay(1500L)
+                println("@@ Finished eating $it")
+            }
+//                flow1.flatMapConcat { value ->
+//                    flow {
+//                        emit(value + 1)
+//                        delay(500L)
+//                        emit(value + 2)
+//                    }
+//                }.collect { value ->
+//                    println("@@collected value is: $value")
+//                }
+//            val reduceResult = countDownFlow
+//                .fold(100) { acc, curr ->
+//                    acc + curr
+//                }
+//
+//            println("@@The count is $reduceResult")
+//            countDownFlow
+//                .filter { time ->
+//                    time % 2 == 0
+//                }
+//                .map { time ->
+//                    time * time
+//                }
+//                .collect { time ->
+//                println("@@The current time is $time")
+//            }
+
+//            val count = countDownFlow
+//                .count {
+//                    it % 2 == 0
+//                }
+//
+//            println("@@The count is $count")
+
+            // latsetLatest
+//            countDownFlow.collectLatest { time ->
+//                // prints only the last value and others are cancelled
+//                delay(1500L)
+//                println("The current time is $time")
+//            }
         }
     }
 }
